21bd109972007f70c0ff1c5ec007dca5807e4fcb
godlatro <godlatro1@gmail.com>
2019-06-13 15:39:36 +0300

    v1.1

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # Graphql-files-merge-JS
 Merge All *.graphql files in dir in JS
+My merger dont need any dependency (only [fs]), small and simple;
+
+# WTF?
+
+You can use this module to merge all your `*.graphql` files for `Apollo Graphql Client`
+
+## Example Schemas:
+
+/*user.graphql*/
+### type User {
+###   name: String #comment
+###   password: String
+### }
+### 
+### type Query {
+###   users: [User]
+### }
+### type Mutation {
+###   user(name: String): User
+### }
+
+/*books.graphql*/
+### type Book {
+###   name: String #comment
+###   author: [User]
+### }
+### #comment
+### type Query {
+###   books: [Book]
+### }
+### type Mutation {
+###   book(name: String): Book
+### }
+
+/*Exported schema after merge*/
+### type User {
+###   name: String
+###   password: String
+### }
+### 
+### type Book {
+###   name: String
+###   author: [User]
+### }
+### 
+### type Query {
+###   users: [User]
+###   books: [Book]
+### }
+### type Mutation {
+###   user(name: String): User
+###   book(name: String): Book
+### }
+
+
+## How to Use
+
+You can donwload and unzip `index.js` to your graphql dir, rename it as you wish, like `GMerger.js`
+then import it in js.
+
+### `const { gql } = require('apollo-server');`
+### `const {merger} = require('./GMerger');`
+### `const typeDefs = merger();`
+### `module.exports.typeDefs = gql(typeDefs);`
+
+## options
+
+### `const typeDefs = merger({ debug: true, type: 'graphql', dir: '/usr/node/graphql' });`
+
+#### *debug* - show result in console.log --- *true*
+#### *type* - choose file types, default is `*.graphql`, but if you want reanme your files in other type, like `*.g` or `*.gql`, you can set this
+#### *dir* - choose scan dir with `*.graphql` files
+
+## На русском
+
+это маленький модуль который не имеет внешних зависимостей, и который можно подключить как JS файл или как NPM зависимость
+С помощью этого модуля можно объединить все `*.graphql` файлы для импорта в клиент `Apollo Graphql Client`
---
+++ index.js
@@ -0,0 +1,70 @@
+/* Godlatro Graphql merger v.1.1 */
+const { readFileSync, readdirSync } = require('fs');
+const merger = (options) => {
+
+  const Debug = options && options.debug && options.debug || false;
+  const Dir = options && options.dir && options.dir || __dirname || './';
+  const FType = options && options.type && options.type || 'graphql';
+
+  const Querys = [];
+  const Muts = [];
+  const files = readdirSync(Dir)
+  .filter(s => s.endsWith(`.${FType}`))
+  .map(file => {
+    if(!file) return null;
+    let Cont = readFileSync(`${Dir}/${file}`, { encoding: 'utf8' })
+    .replace(/\t/g, " ")
+    .replace(/\t\t/g, " ")
+    .replace(/  /g, " ")
+    .replace(/\s\s/g, "\n")
+    .replace(/\r\n/g, "\n")
+    .replace(/\n\n/g, "\n")
+    .replace(/^\s$\s/g, "")
+    .replace(/^ /g, "")
+    .replace(/^\s/g, "");
+    let Query = Cont.match(/(type[\s]+Query[\s]+{)[\s\S]*?}/g);
+    let Mutation = Cont.match(/(type[\s]+Mutation[\s]+{)[\s\S]*?}/g);
+    Cont = Cont.replace(/#.*/gi,'');
+    if(Debug){
+      console.log('merger debug for file', file)
+      console.log('merger Qery =',Query)
+      console.log('merger Mutation =',Mutation)
+      console.log('merger file debug end')
+    }
+    if(!!Query){
+      Query.forEach(e => {
+        let S = e.replace(/type[\s]+Query[\s]+{/g,'')
+        .replace(/}/g,'');
+        Querys.push(S);
+      });
+    }
+    if(!!Mutation){
+      Mutation.forEach(e => {
+        let S = e.replace(/type[\s]+Mutation[\s]+{/g,'')
+        .replace(/}/g,'');
+        Muts.push(S);
+      });
+
+    }
+
+    Cont = Cont.replace(/(type[\s]+Query[\s]+{)[\s\S]*?}/g,'')
+    .replace(/(type[\s]+Mutation[\s]+{)[\s\S]*?}/g, '');
+    return Cont;
+  })
+  .join('');
+
+  if(!files) return null;
+  const Q = `\n type Query{${Querys.join('')}} \n`;
+  const M = `\n type Mutation{${Muts.join('')}} \n`;
+  const typeDefs = `${files} \n ${Querys && Querys.length && Q} \n ${Muts && Muts.length && M}`;
+  
+  if(Debug){
+    console.log('merger debug for typedefs')
+    console.log(typeDefs)
+    console.log('merger end')
+  }
+
+  return typeDefs;
+}
+
+module.exports.merger = merger;
